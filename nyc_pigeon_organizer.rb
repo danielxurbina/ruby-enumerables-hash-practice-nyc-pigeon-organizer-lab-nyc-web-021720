bf94f175fec5cd7760161b9cea241c086c183ea7
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 08:02:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,20 @@
 def nyc_pigeon_organizer(data)
-  empty_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
-        if !empty_hash[name]
-          empty_hash[name] = {}
-        end
-        
-        if !empty_hash[name][key]
-          empty_hash[name][key] = []
-        end
-        
-        empty_hash[name][key] << new_value.to_s
-        
+empty_hash = {}
+data.each do |key, value|
+  value.each do |new_value, names|
+    names.each do |name|
+      if !empty_hash[name]
+        empty_hash[name] = {}
       end
-    end  
-  end
-  empty_hash
+        
+      if !empty_hash[name][key]
+        empty_hash[name][key] = []
+      end
+        
+      empty_hash[name][key] << new_value.to_s
+        
+    end
+  end  
+end
+empty_hash
 end
